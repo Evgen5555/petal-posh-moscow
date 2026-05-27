1b32013c840f41dd11128ebe8ad068847f8c5df1
Евгения <106427544+Evgen5555@users.noreply.github.com>
2026-05-27 17:52:36 +0300

    Refactor App component structure and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { CartProvider } from "@/context/CartContext";
-import { StoreProvider } from "@/context/StoreContext";
-import { SiteContentProvider } from "@/context/SiteContentContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Catalog from "./pages/Catalog";
-import ProductDetail from "./pages/ProductDetail";
-import Cart from "./pages/Cart";
-import Delivery from "./pages/Delivery";
-import Privacy from "./pages/Privacy";
-import Offer from "./pages/Offer";
-import Reviews from "./pages/Reviews";
-import BirthdayPromo from "./pages/BirthdayPromo";
-import Admin from "./pages/Admin";
-import AdminLogin from "./pages/AdminLogin";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -24,30 +11,15 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <StoreProvider>
-        <SiteContentProvider>
-        <CartProvider>
-          <Toaster />
-          <Sonner />
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/catalog" element={<Catalog />} />
-              <Route path="/product/:id" element={<ProductDetail />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/delivery" element={<Delivery />} />
-              <Route path="/privacy" element={<Privacy />} />
-              <Route path="/offer" element={<Offer />} />
-              <Route path="/reviews" element={<Reviews />} />
-              <Route path="/birthday" element={<BirthdayPromo />} />
-              <Route path="/admin/login" element={<AdminLogin />} />
-              <Route path="/admin" element={<Admin />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        </CartProvider>
-        </SiteContentProvider>
-      </StoreProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter basename="/petal-posh-moscow">
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* Catch-all route for 404s */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
