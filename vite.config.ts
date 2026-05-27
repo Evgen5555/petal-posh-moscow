7522763033f12ee90972c5d3076f39834f476b00
Евгения <106427544+Evgen5555@users.noreply.github.com>
2026-05-27 16:46:07 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+ base: '/petal-posh-moscow/',
   server: {
     host: "::",
     port: 8080,
